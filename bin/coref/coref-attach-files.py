5744b11f1457bfb905faa2ad8124e09a828710dc
Robert Elwell <robert@wikia-inc.com>
2012-12-14 11:32:37 -0800

    update core writer and attacher to work as a pipeline -- etl is next

--- bin/coref/coref-attach-files.py
+++ bin/coref/coref-attach-files.py
@@ -1,31 +1,64 @@
 # -*- coding: utf-8 *-*
 """
-Writes Solr documents to their own flat files as part of a coref resolution (and more) pipeline
+Passes raw files to Stanford CoreNLP for processing
 """
 
 from optparse import OptionParser
-from time import sleep
-import os
+import os, shutil, subprocess
 
 parser = OptionParser()
 parser.add_option("-w", "--wiki", dest="wiki", action="store", default=None,
                   help="Specifies the wiki in Solr")
-parser.add_option("-p", "--poll", dest="dest", action="store", default="/tmp/flat_files",
+parser.add_option("-p", "--poll", dest="poll", action="store", default="/tmp/nlp_raw",
                   help="The target path to poll for files")
-parser.add_option("-d", "--dest", dest="dest", action="store", default="/tmp/processed_files",
+parser.add_option("-t", "--temp", dest="temp", action="store", default="/tmp/nlp_processing",
+                  help="The destination path for files being processed")
+parser.add_option("-d", "--dest", dest="dest", action="store", default="/tmp/nlp_processed",
                   help="The destination path for processed files")
+parser.add_option("-T", "--threads", dest="threads", action="store", default="5",
+                  help="Number of threads for CoreNLP to use")
+#TODO: change this path to something else
+parser.add_option("-n", "--nlp-path", dest="nlppath", action="store", default="/Users/relwell/Desktop/stanford-corenlp-full-2012-11-12",
+                  help="Number of threads for CoreNLP to use")
 
 (options, args) = parser.parse_args()
 
-lockfilepath = '%s/attacher.LOCK' % (options.poll)
+LOCKFILE = 'LOCK'
+lockfilepath = '%s/%s' % (options.poll, LOCKFILE)
+TEMPDIR = options.temp+'/'+str(os.getpid())
 
+if not os.path.exists(options.dest):
+    os.makedirs(options.dest)
+
+"""
+Move a folder to a temp directory,
+configure and call the tagger subprocess,
+and delete the temp directory
+"""
+def attach(folder):
+    args = ['java', '-cp']
+    jarfiles = ['stanford-corenlp-1.3.4.jar', 'stanford-corenlp-1.3.4-models.jar', 'xom.jar', 'joda-time.jar', 'jollyday.jar']
+    args.append(':'.join([options.nlppath+'/'+jarfile for jarfile in jarfiles]))
+    args.append('-Xmx3g')
+    args.append('edu.stanford.nlp.pipeline.StanfordCoreNLP')
+    args.append('-annotators')
+    args.append('tokenize,ssplit,pos,lemma,ner,parse,dcoref')
+    args.append('-file')
+    args.append(TEMPDIR)
+    args.append('-threads')
+    args.append(options.threads)
+    args.append('-replaceExtension')
+    args.append('-outputDirectory')
+    args.append(options.dest)
+    shutil.move(folder, TEMPDIR)
+    subprocess.call(args)
+    shutil.rmtree(TEMPDIR)
+
+"""
+Recursively iterate through the target polling folder
+for unlocked, terminal folders
+"""
 while True:
-    listed = os.listdir(options.poll)
-    if len(listed) > 0 and 'attacher.LOCK' not in listed:
-        lockfile = open(lockfilepath, 'w')
-        lockfile.write('')
-        lockfile.close()
-        # move folder someplace else based guid so multiple pollers can eventually be dispatched
-        os.remove(lockfilepath)
-        # call stanford stuff
-    sleep(10)
+    for folder, subs, files in os.walk(options.poll):
+        if len(subs) == 0 and LOCKFILE not in files:
+            attach(folder)
--- bin/coref/coref-write-files.py
+++ bin/coref/coref-write-files.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 *-*
 """
 Writes Solr documents to their own flat files as part of a coref resolution (and more) pipeline
+Throughput on this one is pretty fast, because it's just writing raw text
 """
 
 from optparse import OptionParser
@@ -11,7 +12,7 @@ import os
 parser = OptionParser()
 parser.add_option("-w", "--wiki", dest="wiki", action="store", default=None,
                   help="Specifies the wiki in Solr")
-parser.add_option("-d", "--dest", dest="dest", action="store", default="/tmp/flat_files",
+parser.add_option("-d", "--dest", dest="dest", action="store", default="/tmp/nlp_raw",
                   help="The destination path of files")
 
 (options, args) = parser.parse_args()
@@ -23,15 +24,26 @@ else:
 
 conn = SolrConnection('http://search-s10.prod.wikia.net:8983/solr')
 
-response = conn.query(query, fields=['html_en','nolang_txt','html', 'title', 'title_en', 'id'])
+response = conn.query(query, fields=['html_en','nolang_txt','html', 'title', 'title_en', 'id'], rows=100)
 paginator = SolrPaginator(response)
 
+def initialize_dir(page):
+    paths = [options.dest, options.wiki, str(page)]
+    fullpath = ''
+    for path in paths:
+        fullpath += path + '/'
+        if not os.path.exists(fullpath):
+            os.mkdir(fullpath)
+    return fullpath
+
 for page in paginator.page_range:
-    for doc in paginator.page(page).object_list:
-        text = doc.get('html_en', doc.get('nolang_txt', doc.get('html')))
-        title = doc.get('title_en', doc.get('title', doc['id']))
-        while 'attacher.LOCK' in os.listdir(options.dest):
-            sleep(0.5)
-        f = open('%s/%s' % (options.dest, doc['id']), 'w')
-        f.write("%s\n%s" % (title, text))
-        f.close()
+    pagedir = initialize_dir(page)
+    lockfilepath = pagedir+'/LOCK'
+    with open(lockfilepath, 'w') as lockfile:
+        for doc in paginator.page(page).object_list:
+            text = doc.get('html_en', doc.get('nolang_txt', doc.get('html')))
+            title = doc.get('title_en', doc.get('title', doc['id']))
+            f = open('%s/%s.raw' % (pagedir, doc['id']), 'w')
+            f.write("%s\n%s" % (title.encode('utf8'), text.encode('utf8')))
+            f.close()
+    os.remove(lockfilepath)
