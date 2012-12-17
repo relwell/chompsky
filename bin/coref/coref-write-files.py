8f15ffbce716d379ac85e81544c9a584390af7fd
Robert Elwell <robert@wikia-inc.com>
2012-12-17 15:12:56 -0800

    added query-based option for data extraction

--- bin/coref/coref-write-files.py
+++ bin/coref/coref-write-files.py
@@ -14,21 +14,32 @@ parser.add_option("-w", "--wiki", dest="wiki", action="store", default=None,
                   help="Specifies the wiki in Solr")
 parser.add_option("-d", "--dest", dest="dest", action="store", default="/tmp/nlp_raw",
                   help="The destination path of files")
+parser.add_option("-q", "--query", dest="query", action="store", default=None,
+                  help="Specifies a query instead of using a specific wiki")
 
 (options, args) = parser.parse_args()
 
+query = ''
+
 if options.wiki:
     query = 'host:%s' % (options.wiki)
-else:
+elif not options.query:
     raise Exception('A wiki is required, passed as host name')
 
+if options.query:
+    query += ' '+options.query
+
+specifier = options.wiki if options.wiki else str(os.getpid())
+
 conn = SolrConnection('http://search-s10.prod.wikia.net:8983/solr')
+
+print query
 
 response = conn.query(query, fields=['html_en','nolang_txt','html', 'title', 'title_en', 'id'], rows=100)
 paginator = SolrPaginator(response)
 
 def initialize_dir(page):
-    paths = [options.dest, options.wiki, str(page)]
+    paths = [options.dest, specifier, str(page)]
     fullpath = ''
     for path in paths:
         fullpath += path + '/'
